2ad48b36a3238004d49187e371034e4d000b8fcb
Robson Santos <robfds@gmail.com>
2025-03-17 00:23:55 -0300

    test([Exercism][Go][Vehicle Purchase]): fixing some broken tests

--- exercism/go/vehicle-purchase/vehicle_purchase.go
+++ exercism/go/vehicle-purchase/vehicle_purchase.go
@@ -8,10 +8,10 @@ func NeedsLicense(kind string) bool {
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
 	if option1 > option2 {
-		return option1
+		return option2 + " is clearly the better choice."
 	}
 
-	return option2
+	return option1 + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
