fc2f84faf7cfe21e4665f74a9a0b2a5077538c7a
Robson Santos <robfds@gmail.com>
2025-03-27 18:13:38 -0300

    feat([Exercism][Go][Animal Magic]): add solution to the Generate wand energy challenge

--- exercism/go/animal-magic/animal_magic.go
+++ exercism/go/animal-magic/animal_magic.go
@@ -9,7 +9,7 @@ func RollADie() int {
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return rand.Float64() * 12.0
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
