cd7466c592a873d2af90981034fae406006bcc74
Robson Santos <robfds@gmail.com>
2025-03-28 22:23:21 -0300

    feat([Exercism][Go][Bird Watcher]): finish birdwatcher challenges

--- exercism/go/bird-watcher/bird_watcher.go
+++ exercism/go/bird-watcher/bird_watcher.go
@@ -14,11 +14,23 @@ func TotalBirdCount(birdsPerDay []int) int {
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	start := (week - 1) * 7
+	end := start + 7
+	count := 0
+
+	for i := start; i < end; i++ {
+		count += birdsPerDay[i]
+	}
+
+	return count
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i = i + 2 {
+		birdsPerDay[i] += 1
+	}
+
+	return birdsPerDay
 }
