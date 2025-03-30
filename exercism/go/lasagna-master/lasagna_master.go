c888c35650265fe22cbde80066673d6d298cdec0
Robson Santos <robfds@gmail.com>
2025-03-29 22:59:35 -0300

    feat(exercism, go): WIP lasgna master

---
+++ exercism/go/lasagna-master/lasagna_master.go
@@ -0,0 +1,43 @@
+package lasagna
+
+func countElements[T comparable](arr []T, item T) int {
+	counter := 0
+
+	for k := 0; k < len(arr); k++ {
+		if arr[k] == item {
+			counter += 1
+		}
+	}
+
+	return counter
+}
+
+// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, avg_prep_timer int) int {
+	if avg_prep_timer > 0 {
+		return len(layers) * avg_prep_timer
+	} else {
+		return len(layers) * 2
+	}
+}
+
+// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	amountNoodles := countElements(layers, "noodles")
+	amountSauce := countElements(layers, "sauce")
+
+	return amountNoodles * 50, float64(amountSauce) * 0.2
+}
+
+// TODO: define the 'AddSecretIngredient()' function
+
+// TODO: define the 'ScaleRecipe()' function
+
+// Your first steps could be to read through the tasks, and create
+// these functions with their correct parameter lists and return types.
+// The function body only needs to contain `panic("")`.
+//
+// This will make the tests compile, but they will fail.
+// You can then implement the function logic one by one and see
+// an increasing number of tests passing as you implement more
+// functionality.
