b96cf5495fdccffa63e5ba5e0c3f3fc0f9862dea
Robson Santos <robfds@gmail.com>
2025-04-01 23:31:14 -0300

    feat([Exercism][Go][Interest Is Interesting]): add helper function to convert percent values into decimal form

--- exercism/go/interest-is-interesting/interest_is_interesting.go
+++ exercism/go/interest-is-interesting/interest_is_interesting.go
@@ -1,5 +1,9 @@
 package interest
 
+func asDecimal(value float32) float64 {
+	return float64(value / 100.0)
+}
+
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
 	switch {
@@ -16,12 +20,12 @@ func InterestRate(balance float64) float32 {
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	return balance * (float64(InterestRate(balance)) / 100.0)
+	return balance * asDecimal(InterestRate(balance))
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	return balance * (1.0 + float64(InterestRate(balance))/100.0)
+	return balance * (1.0 + asDecimal(InterestRate(balance)))
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
