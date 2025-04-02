232a970da4714dabc7fca2efa2c7c41f984b834c
Robson Santos <robfds@gmail.com>
2025-04-01 22:31:27 -0300

    feat([Exercism][Go][Interest Is Interesting): Implement  function

--- exercism/go/interest-is-interesting/interest_is_interesting.go
+++ exercism/go/interest-is-interesting/interest_is_interesting.go
@@ -21,7 +21,7 @@ func Interest(balance float64) float64 {
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance * (1.0 + float64(InterestRate(balance))/100.0)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
